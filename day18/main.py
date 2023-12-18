0cca8fa9aba75b82a33c8845fefb48d346748373
Nolanogenn <gnolano@gmail.com>
2023-12-18 09:15:34 -0500

    cleaner code

--- day18/main.py
+++ day18/main.py
@@ -12,36 +12,21 @@ def area(x,y):
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
 def part1(inputfile):
-    graph = {}
     pos = (0,0)
-    graph[pos] = '#'
+    points = [pos]
+    s = 0
     for line in inputfile:
         d, m, _ = line.split()
         d_tuple = dirs[d]
-        for j in range(int(m)):
-            pos = (
-                pos[0]+d_tuple[0], pos[1]+d_tuple[1]
-                )
-            graph[pos] = '#'
-    s = len(graph)
-    
-    x = [x[0] for x in graph]
-    y = [x[1] for x in graph]
+        pos = (
+            pos[0]+d_tuple[0]*int(m), pos[1]+d_tuple[1]*int(m)
+            )
+        s += int(m)
+        points.append(pos)
 
-#    max_x = max(x)
-#    min_x = min(x)
-#    max_y = max(y)
-#    min_y = min(y)
-#
-#    graph_normalized = {}
-#    for node in graph:
-#        graph_normalized[(node[0]-min_x, node[1]-min_y)] = graph[node]
-#
-#    normalized_x = [x[0] for x in graph_normalized]
-#    normalized_y = [x[1] for x in graph_normalized]
-#    max_x = max(normalized_x)
-#    max_y = max(normalized_y)
-#
+    x = [x[0] for x in points]
+    y = [x[1] for x in points]
+
     a = area(y, x)
     A = a+1-s//2
     return A+s
@@ -79,7 +64,7 @@ def part2(inputfile):
     return A+s
 
 if __name__ == '__main__':
-    inputfile = open('day18.txt').readlines()
+    inputfile = open('day18test.txt').readlines()
     sol1 = part1(inputfile)
     print(f"Solution for part 1: {sol1}")
     sol2 = part2(inputfile)
