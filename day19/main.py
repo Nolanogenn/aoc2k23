f41726c7cd8e28fcc125add3201e4b404d7b9e89
nolano_g <gennaro.nolano@maredigital.it>
2023-12-19 17:58:36 +0100

    day19

--- day19/main.py
+++ day19/main.py
@@ -1,20 +1,21 @@
 import re
+import pprint
 
 def part1(workflows, parts):
     nodes = {}
     for step in workflows:
-        nodes[step[0]] = {}
+        nodes[step[0]] = []
         for branch in step[1]:
             if '<' in branch:
                 data = branch.split('<')
                 num,node = data[1].split(':')
-                nodes[step[0]][data[0]] = {'lower' : int(num), 'child' : node}
+                nodes[step[0]].append((data[0],'lower',int(num),node))
             elif '>' in branch:
                 data = branch.split('>')
                 num,node = data[1].split(':')
-                nodes[step[0]][data[0]] = {'higher' : int(num), 'child' : node}
+                nodes[step[0]].append((data[0],'higher',int(num),node))
             else:
-                nodes[step[0]]['else'] = branch
+                nodes[step[0]].append(('else', branch))
     accepted = []
     for part in parts:
         d_part = {
@@ -23,29 +24,61 @@ def part1(workflows, parts):
         }
         pos = 'in' 
         while pos not in ['R', 'A']:
-            check_list = [x for x in nodes[pos] if x != 'else']
-            for tocheck in check_list:
-                val = d_part[tocheck]
-                if 'lower' in nodes[pos][tocheck]:
-                    if val < nodes[pos][tocheck]['lower']:
-                        pos = nodes[pos][tocheck]['child']
-                        break
-                else:
-                    if val > nodes[pos][tocheck]['higher']:
-                        pos = nodes[pos][tocheck]['child']
-                        break
+            check_list = [n for n in nodes[pos] if len(n) > 2]
+            cs = [
+                (d_part[c[0]] < c[2],c[3]) 
+                if c[1] == 'lower' 
+                else (d_part[c[0]] > c[2],c[3]) 
+                for c in check_list]
+            pos2 = [x[1] for x in cs if x[0]]
+            if pos2:
+                pos = pos2[0]
             else:
-                pos = nodes[pos]['else']
+                pos = nodes[pos][-1][-1]
         if pos == 'A':
             accepted.append(sum(d_part.values()))
     return sum(accepted)
-def part2(workflows, parts):
+
+def part2(workflows):
+    nodes = {}
+    for step in workflows:
+        nodes[step[0]] = []
+        for branch in step[1]:
+            if '<' in branch:
+                data = branch.split('<')
+                num,node = data[1].split(':')
+                nodes[step[0]].append((data[0],'lower',int(num),node))
+            elif '>' in branch:
+                data = branch.split('>')
+                num,node = data[1].split(':')
+                nodes[step[0]].append((data[0],'higher',int(num),node))
+            else:
+                nodes[step[0]].append(('else', branch))
+    pos = 'in'
+    queue = [(pos, {"x":4000, "m":4000, "a":4000, "s":4000})]
+    v = 0
+    while queue:
+        x = queue[0]
+        pos = x[0]
+        ns = x[1]
+        branches = nodes[pos]
+        for branch in branches:
+            filtered = dict(ns)
+            if x[0] != 'else':
+                tofilter = branch[0]
+                direction = branch[-1]
+                filtered[tofilter] = ns[tofilter]-branch[2]
+                new_branch = (direction, branch[2])
+                queue.append(new_branch)
+            else:
+                direction = branch[-1]
+                queue.append((direction, filtered))
     pass
 
 if __name__ == '__main__':
     pattern = r'([a-z]*){(.*)}'
     pattern_part = r'([xmas])=([0-9]+)'
-    workflows, parts = open('day19.txt').read().split('\n\n')
+    workflows, parts = open('day19test.txt').read().split('\n\n')
     workflows = workflows.strip().split()
     workflows = [re.search(pattern, workflow).groups() for workflow in workflows]
     workflows = [(x[0],x[1].split(',')) for x in workflows]
@@ -53,5 +86,5 @@ if __name__ == '__main__':
     parts = [re.findall(pattern_part, part) for part in parts]
     sol1 = part1(workflows, parts)
     print(f"Solution for part 1: {sol1}")
-    sol2 = part2(workflows, parts)
+    sol2 = part2(workflows)
     print(f"Solution for part 2: {sol2}")
